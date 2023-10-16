f58dc11667bf7d1eb071ad4108bcd2261282fb9f
adamsHillzz <adamsHillzz@yandex.ru>
2023-10-16 18:10:27 +0300

    Start project

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold underline">NextJS 13</h1>
+      <button className="btn">Click Me</button>
+    </main>
+  )
+}
